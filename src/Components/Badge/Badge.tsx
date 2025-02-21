3ea54733120c9a2d0161cce221da767c1922140b
Artem <96584594+LifelessShirt@users.noreply.github.com>
2025-02-21 18:15:48 +0100

    New badge and styles

--- src/Components/Badge/Badge.tsx
+++ src/Components/Badge/Badge.tsx
@@ -6,7 +6,12 @@ interface BadgeProps {id:string, type?:string}
 const Badge = ({id, type}:BadgeProps) => {
     const arr:{id:string, name:string, description:string} | undefined = BadgeList.find(item => item.id === id);
     return (
-        <div className={`${Style.badge} ${Style[`badge-`+arr?.name]} ${Style[type || ""]}`}></div>
+        <div className={`${Style.badge} ${Style[`badge-`+arr?.name.split(" ").join("-")]} ${Style[type || ""]}`}>
+            <div className={Style.tooltip}>
+                <div className={Style.tooltipTitle}>{arr?.name}</div>
+                <div className={Style.tooltipDescription}>{arr?.description}</div>
+            </div>
+        </div>
     );
 }
 export default Badge;
--- src/Components/Badge/BadgeList.tsx
+++ src/Components/Badge/BadgeList.tsx
@@ -1,6 +1,7 @@
 export const BadgeList = [
-    {id: '0', name: "Hawkeye", description: "Wow! Are you REALY reads incoming mail?"},
-    {id: '1', name: "Explorer", description: "Take 20,000 steps."},
-    {id: '2', name: "Dragonborn", description: "Slay ender dragon 10 times."},
-    {id: '3', name: "Fighter", description: "Win a PvP fight."}
+    {id: '0', name: "Season One", description: "He was on Season One. A true legend."},
+    {id: '1', name: "Hawkeye", description: "Wow! Are you REALY reads incoming mail?"},
+    {id: '2', name: "Explorer", description: "Take 20,000 steps."},
+    {id: '3', name: "Dragonborn", description: "Slay ender dragon 10 times."},
+    {id: '4', name: "Fighter", description: "Win a PvP fight."}
 ];
